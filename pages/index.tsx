403c56c1e49758efb8ad6237cd6d93b51478b846
Ryszard-Penkala <ryszard.penkala@gmail.com>
2023-01-08 07:31:09 +0100

    added Layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,9 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-          <Header/>
         <h1>Hello</h1>
-          <Footer/>
       </main>
 
     </>
---
+++ pages/layouts/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import styles from "./Layout.module.scss";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+
+type Props = {
+    children: JSX.Element
+}
+
+const Layout = ({ children }: Props) => {
+    return(
+        <div className={styles.content}>
+            <Header />
+            {children}
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout;
