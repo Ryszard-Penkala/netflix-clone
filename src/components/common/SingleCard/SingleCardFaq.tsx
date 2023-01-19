bd17758a95cdba388df4a7fb92d6c241e5a6203f
Ryszard-Penkala <ryszard.penkala@gmail.com>
2023-01-19 02:54:28 +0100

    added all faq list items

--- src/components/common/SingleCard/SingleCardFaq.tsx
+++ src/components/common/SingleCard/SingleCardFaq.tsx
@@ -24,6 +24,77 @@ const SingleCardFaq = () => {
                             </span>
                         </div>
                     </li>
+                    <li className={styles.faqListItem}>
+                        <button className={styles.faqQuestion}>
+                            How much does Netflix cost??
+                            <svg viewBox="0 0 26 26" className={`${styles.svgIcon} ${styles.svgClosed}`} focusable={true}>
+                                <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
+                            </svg>
+                        </button>
+                        <div className={styles.faqAnswer}>
+                            <span>
+                                Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from 29 zł to 60 zł a month. No extra costs, no contracts.
+                            </span>
+                        </div>
+                    </li>
+                    <li className={styles.faqListItem}>
+                        <button className={styles.faqQuestion}>
+                            Where can I watch?
+                            <svg viewBox="0 0 26 26" className={`${styles.svgIcon} ${styles.svgClosed}`} focusable={true}>
+                                <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
+                            </svg>
+                        </button>
+                        <div className={styles.faqAnswer}>
+                            <span>
+                                Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.                                <br/>
+                                <br/>
+                                <br/>
+                                You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.
+                            </span>
+                        </div>
+                    </li>
+                    <li className={styles.faqListItem}>
+                        <button className={styles.faqQuestion}>
+                            How do I cancel?
+                            <svg viewBox="0 0 26 26" className={`${styles.svgIcon} ${styles.svgClosed}`} focusable={true}>
+                                <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
+                            </svg>
+                        </button>
+                        <div className={styles.faqAnswer}>
+                            <span>
+                                Netflix is flexible. There are no pesky contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime.
+                            </span>
+                        </div>
+                    </li>
+                    <li className={styles.faqListItem}>
+                        <button className={styles.faqQuestion}>
+                            What can I watch on Netflix?
+                            <svg viewBox="0 0 26 26" className={`${styles.svgIcon} ${styles.svgClosed}`} focusable={true}>
+                                <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
+                            </svg>
+                        </button>
+                        <div className={styles.faqAnswer}>
+                            <span>
+                                Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.
+                            </span>
+                        </div>
+                    </li>
+                    <li className={styles.faqListItem}>
+                        <button className={styles.faqQuestion}>
+                            Is Netflix good for kids?
+                            <svg viewBox="0 0 26 26" className={`${styles.svgIcon} ${styles.svgClosed}`} focusable={true}>
+                                <path d="M10.5 9.3L1.8 0.5 0.5 1.8 9.3 10.5 0.5 19.3 1.8 20.5 10.5 11.8 19.3 20.5 20.5 19.3 11.8 10.5 20.5 1.8 19.3 0.5 10.5 9.3Z"></path>
+                            </svg>
+                        </button>
+                        <div className={`${styles.faqAnswer} ${styles.faqAnswerClosed}`}>
+                            <span>
+                                The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space.
+                                <br/>
+                                <br/>
+                                Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.
+                            </span>
+                        </div>
+                    </li>
                 </ul>
             Text
             </div>
