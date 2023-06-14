6449e7a0f9ca2699803712fa82353110a5f4d768
Ryszard-Penkala <ryszard.penkala@gmail.com>
2023-06-14 23:20:25 +0200

    added SingleCard email section

--- src/components/common/SingleCard/SingleCard.tsx
+++ src/components/common/SingleCard/SingleCard.tsx
@@ -28,6 +28,31 @@ const SingleCard = () => {
                             Watch anywhere. Cancel anytime.
                         </p>
                     </div>
+                    <div className={styles.emailItem}>
+                        <div className={styles.emailItemContainer}>
+                            <form className={styles.emailForm}>
+                                <h3 className={styles.emailHeader}>
+                                    Ready to watch? Enter your email to create or restart your membership.
+                                </h3>
+                                <div className={styles.emailContainer}>
+                                    <div className={styles.emailBar}>
+                                        <label htmlFor="emailInputField" className={styles.emailLabel}>
+                                            Email address
+                                        </label>
+                                        <div className={styles.emailInputContainer}>
+                                            <input type="email" autoComplete="email" minLength={5} maxLength={50} name="email" id="emailInputField"/>
+                                            <div className={styles.emailBackgroundStyle}/>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            </form>
+
+
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
