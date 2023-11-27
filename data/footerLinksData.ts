60df8512bc57f0b651ca281ba0cfd7d5a1bee976
Ryszard-Penkala <ryszard.penkala@gmail.com>
2023-11-28 00:18:20 +0100

    footer links adress change

--- data/footerLinksData.ts
+++ data/footerLinksData.ts
@@ -6,74 +6,92 @@ export type FooterLinkDataProps = {
 export const FooterLinksData: FooterLinkDataProps[] = [
     {
         footerData: "FAQ",
-        footerDataLink: "https://help.netflix.com/en/node/412",
+        // footerDataLink: "https://help.netflix.com/en/node/412",
+        footerDataLink: "#",
     },
     {
         footerData: "Help Center",
-        footerDataLink: "https://help.netflix.com/en/",
+        // footerDataLink: "https://help.netflix.com/en/",
+        footerDataLink: "#",
     },
     {
         footerData: "Account",
-        footerDataLink: "https://www.netflix.com/pl-en/login?nextpage=https%3A%2F%2Fwww.netflix.com%2Fyouraccount",
+        // footerDataLink: "https://www.netflix.com/pl-en/login?nextpage=https%3A%2F%2Fwww.netflix.com%2Fyouraccount",
+        footerDataLink: "#",
     },
     {
         footerData: "Media Center",
-        footerDataLink: "https://media.netflix.com/en/",
+        // footerDataLink: "https://media.netflix.com/en/",
+        footerDataLink: "#",
     },
     {
         footerData: "Investor Relations",
-        footerDataLink: "https://ir.netflix.net/ir-overview/profile/default.aspx",
+        // footerDataLink: "https://ir.netflix.net/ir-overview/profile/default.aspx",
+        footerDataLink: "#",
     },
     {
         footerData: "Jobs",
-        footerDataLink: "https://jobs.netflix.com/",
+        // footerDataLink: "https://jobs.netflix.com/",
+        footerDataLink: "#",
     },
     {
         footerData: "Redeem Gift Cards",
-        footerDataLink: "https://www.netflix.com/pl-en/redeem",
+        // footerDataLink: "https://www.netflix.com/pl-en/redeem",
+        footerDataLink: "#",
     },
     {
         footerData: "Buy Gift Cards",
-        footerDataLink: "https://www.netflix.com/gift-cards",
+        // footerDataLink: "https://www.netflix.com/gift-cards",
+        footerDataLink: "#",
     },
     {
         footerData: "Ways to Watch",
-        footerDataLink: "https://devices.netflix.com/en/",
+        // footerDataLink: "https://devices.netflix.com/en/",
+        footerDataLink: "#",
     },
     {
         footerData: "Terms of Use",
-        footerDataLink: "https://help.netflix.com/legal/termsofuse",
+        // footerDataLink: "https://help.netflix.com/legal/termsofuse",
+        footerDataLink: "#",
     },
     {
         footerData: "Privacy",
-        footerDataLink: "https://help.netflix.com/legal/privacy",
+        // footerDataLink: "https://help.netflix.com/legal/privacy",
+        footerDataLink: "#",
     },
     {
         footerData: "Cookie Preferences",
-        footerDataLink: "https://www.netflix.com/pl-en/#",
+        // footerDataLink: "https://www.netflix.com/pl-en/#",
+        footerDataLink: "#",
     },
     {
         footerData: "Corporate Information",
-        footerDataLink: "https://help.netflix.com/legal/corpinfo",
+        // footerDataLink: "https://help.netflix.com/legal/corpinfo",
+        footerDataLink: "#",
     },
     {
         footerData: "Contact Us",
-        footerDataLink: "https://help.netflix.com/en/contactus",
+        // footerDataLink: "https://help.netflix.com/en/contactus",
+        footerDataLink: "#",
     },
     {
         footerData: "Speed Test",
-        footerDataLink: "https://fast.com/",
+        // footerDataLink: "https://fast.com/",
+        footerDataLink: "#",
     },
     {
         footerData: "Legal Guarantee",
-        footerDataLink: "https://help.netflix.com/en/node/125888",
+        // footerDataLink: "https://help.netflix.com/en/node/125888",
+        footerDataLink: "#",
     },
     {
         footerData: "Legal Notices",
-        footerDataLink: "https://help.netflix.com/legal/notices",
+        // footerDataLink: "https://help.netflix.com/legal/notices",
+        footerDataLink: "#",
     },
     {
         footerData: "Only on Netflix",
-        footerDataLink: "https://www.netflix.com/pl-en/browse/genre/839338",
+        // footerDataLink: "https://www.netflix.com/pl-en/browse/genre/839338",
+        footerDataLink: "#",
     }
 ]
